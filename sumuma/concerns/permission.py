736eaa4cfa04559b64980adff1b88c96df843f9e
uichi <37263474+uichi@users.noreply.github.com>
2022-06-16 21:28:54 +0900

    Restrict registration income and expenditure

--- sumuma/concerns/permission.py
+++ sumuma/concerns/permission.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
+from kakeibo.models import Income, Expenditure
+
 
 class OnlyYouMixin(UserPassesTestMixin):
     """
@@ -9,3 +11,23 @@ class OnlyYouMixin(UserPassesTestMixin):
     def test_func(self):
         user = self.request.user
         return user.pk == self.kwargs['pk'] or user.is_superuser
+
+
+class OnlyYouIncomeMixin(UserPassesTestMixin):
+    """
+    login user only register own income.
+    """
+    raise_exception = True
+
+    def test_func(self):
+        return Income.objects.filter(id=self.kwargs['pk'], user_id=self.request.user.id, deleted=False).exists()
+
+
+class OnlyYouExpenditureMixin(UserPassesTestMixin):
+    """
+    login user only register own expenditure.
+    """
+    raise_exception = True
+
+    def test_func(self):
+        return Expenditure.objects.filter(id=self.kwargs['pk'], user_id=self.request.user.id, deleted=False).exists()
