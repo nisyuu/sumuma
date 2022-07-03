02b2a042ac283d4e2f08b972af8d042b5d54cb5a
uichi <37263474+uichi@users.noreply.github.com>
2022-07-03 17:39:31 +0900

    add todo model to shopping app

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class ToDo(models.Model):
+    name = models.TextField(_('memo'), max_length=100, help_text=_('Please enter the name.'))
+    amount = models.BigIntegerField(_('amount'), blank=True, null=True)
+    event_date = models.DateField(
+        _('event date'),
+        blank=True,
+        null=True,
+        help_text=_('Please enter the event date.')
+    )
+    memo = models.TextField(_('memo'), max_length=1000, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_bought = models.BooleanField(_('is bought'), default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.amount)
