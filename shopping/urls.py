631945c8fd7ba49a02eebcc506d746dd4a7903be
uichi <37263474+uichi@users.noreply.github.com>
2022-07-09 10:55:53 +0900

    add editing todo and deleting todo urls

--- shopping/urls.py
+++ shopping/urls.py
@@ -6,4 +6,6 @@ app_name = 'shopping'
 urlpatterns = [
     path('todo/', views.Top.as_view(), name='top'),
     path('create-todo/', views.CreateToDo.as_view(), name='create_todo'),
+    path('edit-todo/<int:pk>/', views.EditToDo.as_view(), name='edit_todo'),
+    path('delete-todo/<int:pk>/', views.DeleteToDo.as_view(), name='delete_todo'),
 ]
