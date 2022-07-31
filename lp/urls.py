8f43dfa8a651b6dfd4ac116ac0e5b8f114b072cb
uichi <37263474+uichi@users.noreply.github.com>
2022-07-31 16:07:07 +0900

    cache lp general

--- lp/urls.py
+++ lp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views
+from django.views.decorators.cache import cache_page
 
 # from django.views.decorators.cache import cache_page
 
 app_name = 'lp'
 
 urlpatterns = [
-    path('general/', views.General.as_view(), name='general'),
+    path('general/', cache_page(60 * 60 * 2)(views.General.as_view()), name='general'),
 ]
