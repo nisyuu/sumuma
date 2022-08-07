1f0280baf6c58cd4dd4e882f5f0743755c426e7e
uichi <37263474+uichi@users.noreply.github.com>
2022-08-07 10:47:23 +0900

    change admin page path

--- sumuma/urls.py
+++ sumuma/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('x9T5y2sg/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', include('home.urls')),
