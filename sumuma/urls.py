1965f26f7cfed3f448a52da165751c25bf92b93f
uichi <37263474+uichi@users.noreply.github.com>
2023-04-08 23:18:53 +0900

    add basename to router register
    add swagger path

--- sumuma/urls.py
+++ sumuma/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework import routers
 
+from kakeibo.views.api.kakeibo import CategoriesViewSet, IncomesViewSet, ExpendituresViewSet
+
 router = routers.DefaultRouter()
+router.register(r'categories', CategoriesViewSet, basename='categories')
+router.register(r'incomes', IncomesViewSet, basename='incomes')
+router.register(r'expenditures', ExpendituresViewSet, basename='expenditures')
 
 urlpatterns = [
     path('x9T5y2sg/', admin.site.urls),
@@ -31,6 +37,12 @@ urlpatterns = [
     path('budget/', include('budget.urls')),
     path('shopping/', include('shopping.urls')),
     path('lp/', include('lp.urls')),
+    # NOTE: api routers
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # NOTE: swagger
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
