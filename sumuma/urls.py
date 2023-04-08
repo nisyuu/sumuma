18ffcac2ae4520be7cbd46ac7d67f7a9d95421b0
uichi <37263474+uichi@users.noreply.github.com>
2023-04-09 00:18:09 +0900

    add swagger and redoc

--- sumuma/urls.py
+++ sumuma/urls.py
@@ -14,11 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework import routers
 
 from kakeibo.views.api.kakeibo import CategoriesViewSet, IncomesViewSet, ExpendituresViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="sumuma API",
+        default_version='v1',
+        description="sumuma Swagger UI",
+    ),
+    public=True,
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly],
+)
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoriesViewSet, basename='categories')
@@ -41,8 +53,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # NOTE: swagger
-    path('swagger-ui/', TemplateView.as_view(
-        template_name='swagger-ui.html',
-        extra_context={'schema_url': 'openapi-schema'}
-    ), name='swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
