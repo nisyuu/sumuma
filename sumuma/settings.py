d4b08b8fba885ec5f5c131bb35c7d862c65f39df
uichi <37263474+uichi@users.noreply.github.com>
2024-01-27 15:27:53 +0900

    管理画面のURLパスを秘匿する

--- sumuma/settings.py
+++ sumuma/settings.py
@@ -194,3 +194,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50
 }
+
+ADMIN_DASHBOARD_PATH = env('ADMIN_DASHBOARD_PATH')
