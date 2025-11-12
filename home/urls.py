59beaa90b4670d6242cfd954e5f20251086fce54
にしゅう <37263474+nisyuu@users.noreply.github.com>
2025-11-12 21:05:37 +0900

    Revert "ログインできないようにする"

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import cache_page
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.Top.as_view(), name='top'),
+    path('', cache_page(60 * 60 * 2)(views.Top.as_view()), name='top'),
     path('terms_of_service/', views.TermsOfService.as_view(), name='terms_of_service'),
     path('privacy_policy/', views.PrivacyPolicy.as_view(), name='privacy_policy'),
 ]
