ffa8c436512b1163dfc904880a12498df3dc23eb
nisyuu <37263474+nisyuu@users.noreply.github.com>
2025-11-13 16:49:21 +0900

    staticの設定を公式ガイドと同じにする

--- sumuma/settings.py
+++ sumuma/settings.py
@@ -155,13 +155,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = str(PurePath.joinpath(BASE_DIR, 'static'))
-STATICFILES_DIRS = [str(PurePath.joinpath(SUMUMA_PROJECT_DIR, 'static'))]
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+STATIC_URL = '/static/'
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
